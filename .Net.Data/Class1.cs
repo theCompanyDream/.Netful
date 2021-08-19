e4d65547a782104547bb0795e5d6aa928f58c275
Timothy Brantley II <tbrantleyii@gmail.com>
2021-08-19 15:41:29 -0400

    initial checkin

---
+++ .Net.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _Net.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ .Net.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _Net.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
